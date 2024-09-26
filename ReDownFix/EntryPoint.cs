065b35a3dccd79a41128408b369312d7fef6d396
Dinorush <onparnorthstar@gmail.com>
2024-09-25 23:15:12 -0400

    Unload if CConsole present

--- ReDownFix/EntryPoint.cs
+++ ReDownFix/EntryPoint.cs
@@ -1,16 +1,24 @@
 ﻿using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using ModBase.Utils;
 
 namespace ModBase
 {
     [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.0")]
+    [BepInDependency(CC_MODNAME, BepInDependency.DependencyFlags.SoftDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
         public const string MODNAME = "ReDownFix";
-
+        private const string CC_MODNAME = "CConsole";
         public override void Load()
         {
+            if (IL2CPPChainloader.Instance.Plugins.ContainsKey(CC_MODNAME))
+            {
+                DinoLogger.Error("CConsole detected (incompatible). Skipping loading " + MODNAME);
+                return;
+            }
+
             Log.LogMessage("Loading " + MODNAME);
             new Harmony(MODNAME).PatchAll();
             Log.LogMessage("Loaded " + MODNAME);
