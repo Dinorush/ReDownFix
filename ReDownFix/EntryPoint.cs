6d82707296a77a7beeff7e0b7b24e5dedce9d456
Dinorush <onparnorthstar@gmail.com>
2024-09-25 23:02:33 -0400

    Add project files.

---
+++ ReDownFix/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace ModBase
+{
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.0")]
+    internal sealed class EntryPoint : BasePlugin
+    {
+        public const string MODNAME = "ReDownFix";
+
+        public override void Load()
+        {
+            Log.LogMessage("Loading " + MODNAME);
+            new Harmony(MODNAME).PatchAll();
+            Log.LogMessage("Loaded " + MODNAME);
+        }
+    }
+}
---
+++ ReDownFix/PlayerDamagePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace ReDownFix
+{
+    [HarmonyPatch]
+    internal static class PlayerDamagePatch
+    {
+        [HarmonyPatch(typeof(Dam_PlayerDamageBase), nameof(Dam_PlayerDamageBase.OnIncomingDamage))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        private static bool Pre_OnDamage(Dam_PlayerDamageBase __instance, ref bool __result)
+        {
+            __result = true;
+            return __instance.Owner.Alive;
+        }
+    }
+}
